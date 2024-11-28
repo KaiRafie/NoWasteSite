329624cfebef1af8d2c36fbe7bec9f5f639208b9
KAI_RAF <kaisrafie9@gmail.com>
2024-11-27 23:47:16 -0500

    Finish the feature completely by making the whole page getting larger when closing the sidebar and smaller when opening the sidebar

--- script/main.js
+++ script/main.js
@@ -9,16 +9,24 @@ document.querySelectorAll('.collapse-btn').forEach((header) => {
 });
 // to collapse the sidebar as a whole
 document.querySelector('.sidebar-btn').addEventListener('click', function () {
+    //sidebar
     const aside = document.querySelector('aside');
     const sidebarButton = document.querySelector('.sidebar-btn');
 
+    //the right sidecontent
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
 
     aside.classList.toggle('collapsed');
 
     if (aside.classList.contains('collapsed')) {
         sidebarButton.style.left = '0';
+        main.style.marginLeft = '0';
+        footer.style.marginLeft = '0';
     } else {
         sidebarButton.style.left = '157px';
+        main.style.marginLeft = '150px';
+        footer.style.marginLeft = '170px';
     }
     this.classList.toggle('active');
 });
